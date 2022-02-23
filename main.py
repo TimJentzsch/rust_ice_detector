b24c53dd90112fbec95229921d94a85bcdead7e0
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-02-23 10:26:04 +0100

    Incrementally compile commit history

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import re
 import tempfile
 import subprocess
+from typing import List, Tuple
 
-from git import Repo
+from git import Repo, Commit
 
 REPO_URL = "git@github.com:TimJentzsch/stonefish_engine.git"
+
+GITHUB_SSH_REGEX = re.compile(
+    r"^git@github\.com:(?P<organization>[\w_-]+)/(?P<repository>[\w_-]+)\.git$"
+)
+GITHUB_COMMIT_URL = "https://github.com/{organization}/{repository}/commit/{hash}"
+
+
+def get_organization_repository(repo_url: str) -> Tuple[str, str]:
+    """Extract the organization and repository from a repository URL."""
+    match = GITHUB_SSH_REGEX.match(repo_url)
+    if not match:
+        print(f"Invalid repository URL '{repo_url}'. Please use a GitHub SSH URL.")
+        exit(1)
+    return match["organization"], match["repository"]
 
 
 def check_compile_errors(dir_path: str) -> bool:
@@ -21,20 +37,34 @@ def check_compile_errors(dir_path: str) -> bool:
     return output.returncode != 0
 
 
-def process_repo(name: str, repo_url: str) -> None:
+def process_repo(repo_url: str) -> None:
+    organization, repository = get_organization_repository(repo_url)
+
     with tempfile.TemporaryDirectory() as tempdir:
-        print(f"Cloning {name} to file://{tempdir}...")
+        print(f"Cloning {organization}/{repository} to file://{tempdir}...")
         repo = Repo.clone_from(repo_url, tempdir)
-        print(f"Active branch: {repo.active_branch} Dir: {repo.working_dir}")
-        errors = check_compile_errors(tempdir)
-        if errors:
-            print("Errors detected!")
-        else:
-            print("No errors detected.")
+
+        # Get the commits to process
+        commits: List[Commit] = []
+        for commit, _ in zip(repo.iter_commits(), range(20)):
+            commits.append(commit)
+
+        for commit in reversed(commits):
+            print(
+                "Processing ",
+                GITHUB_COMMIT_URL.format(
+                    organization=organization, repository=repository, hash=commit.hexsha
+                ),
+            )
+            errors = check_compile_errors(tempdir)
+            if errors:
+                print("Errors detected!")
+            else:
+                print("No errors detected.")
 
 
 def main():
-    process_repo("Repo", REPO_URL)
+    process_repo(REPO_URL)
 
 
 if __name__ == "__main__":
